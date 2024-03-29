8b2691d9a582cc434834aa3da9aee095f60f3038
Bryan Cauã Arruda Santos <bryanncaua@gmail.com>
2024-03-29 01:05:59 -0300

    Projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Luxuosos from "./components/Luxuosos"
 export default function App() {
   return (
     <main className=" min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex max-w-[920px]">
-      <section className="lg:flex justify-center items-center">
+      <div className="lg:flex justify-center items-center">
         
         <Sedans />
 
@@ -15,7 +15,7 @@ export default function App() {
         <Luxuosos />
 
        
-      </section>
+      </div>
     </main>
   )
   
